ac05f392e429ff39fb9a0c7675a7ed66af9ad316
Ryan Hans <primaryryanhans@gmail.com>
2026-07-04 23:48:06 +0530

    chore: remove unused db and routing boilerplate from main

--- backend/main.go
+++ backend/main.go
@@ -5,22 +5,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
-	"gorm.io/gorm"
 )
-
-type Repository struct {
-	DB *gorm.DB
-}
-
-func (r *Repository) setuproutes(app *fiber.App) {
-	api := app.Group("/api")
-	api.Post("/createQuotation", r.CreateQuotation)
-	api.Post("/createProformaInvoice", r.CreateProformaInvoice)
-	api.Post("/createTaxInvoice", r.CreateTaxInvoice)
-	api.Get("/getQuotation/:id", r.GetQuotation)
-	api.Get("/getProformaInvoice/:id", r.GetProformaInvoice)
-	api.Get("/getTaxInvoice/:id", r.GetTaxInvoice)
-}
 
 func main() {
 	err := godotenv.Load(".env")
@@ -29,17 +14,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	db, err := storage.NewConnection(config)
-
-	if err != nil {
-		log.Fatal("Could not load the database", err)
-	}
 	app := fiber.New()
 
-	r := Repository{
-		DB: db,
-	}
-
-	r.setuproutes(app)
 	app.Listen(":8080")
 }

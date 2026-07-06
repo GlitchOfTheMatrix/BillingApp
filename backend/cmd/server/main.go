6db0d50498248c5ac4a566883080b0fb56cea978
Ryan Hans <primaryryanhans@gmail.com>
2026-07-06 15:06:00 +0530

    refactor: changed dir structure for main.go

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app := fiber.New()
+
+	app.Listen(":8080")
+}
